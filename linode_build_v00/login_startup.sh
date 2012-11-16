f0532078473f6b8493f002c32ec6440373978204
medined <david.medinets@gmail.com>
2012-11-16 13:23:49 -0500

    using a proper login_startup.sh script

---
+++ linode_build_v00/login_startup.sh
@@ -0,0 +1,6 @@
+export DEFAULT_PATH=/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/usr/games
+export ACCUMULO_HOME=/usr/local/accumulo
+export HADOOP_HOME=/usr/local/hadoop
+export JAVA_HOME=/usr/lib/jvm/java-6-openjdk-i386
+export ZOOKEEPER_HOME=/usr/local/zookeeper
+export PATH=$ACCUMULO_HOME/bin:$HADOOP_HOME/bin:$JAVA_HOME/bin:$ZOOKEEPER_HOME/bin:$DEFAULT_PATH
